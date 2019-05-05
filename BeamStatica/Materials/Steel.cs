e662a6f0504bef992c95f3a94d3c4e3d0e4422a4
Konrad Kania <kania.konrad92@gmail.com>
2019-05-05 20:34:49 +0200

    BeamStatica naming.

--- BeamStatica/Materials/Steel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BeamStatica.Materials
-{
-    public class Steel : Material
-    {
-        public Steel() : base(youngModulus: 210, thermalExpansionCoefficient: 0.000012)
-        {
-        }
-    }
-}
--- BeamStatica/Results/Reactions/ShearForce.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BeamStatica.Results.Reactions
-{
-    public class ShearForce : Reaction
-    {
-        public ShearForce(double? position = null) : base(position)
-        {
-        }
-    }
-}
